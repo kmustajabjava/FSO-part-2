c5f2510653fcc175662fb79e51a5ebd55a24066d
KAINAT MUSTAJAB AHMED <115812528+kmustajabjava@users.noreply.github.com>
2023-07-21 22:28:23 +0500

    PhoneBook-2.16-2.17

---
+++ PART2/PhoneBook-2.16-2.17/src/Components/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+    const footerStyle = {
+      color: 'green',
+      fontStyle: 'italic',
+      fontSize: 16
+    }
+    return (
+      <div style={footerStyle}>
+        <br />
+        <em>Phonebook app, @kmustajab2023</em>
+      </div>
+    )
+  }
+  export default Footer
